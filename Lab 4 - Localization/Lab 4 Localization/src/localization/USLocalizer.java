5cad41962ef24b14bc358d62e93d1b564c25c646
xianyizhan <31860732+xianyizhan@users.noreply.github.com>
2017-10-07 11:39:00 -0400

    more set-up code

--- Lab 4 - Localization/Lab 4 Localization/src/localization/USLocalizer.java
+++ Lab 4 - Localization/Lab 4 Localization/src/localization/USLocalizer.java
@@ -1,5 +1,43 @@
 package localization;
 
-public class USLocalizer {
+import lejos.hardware.Button;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.robotics.SampleProvider;
+import localization.USLocalizer.LocalizationType;
 
+public class USLocalizer {
+	public enum LocalizationType { FALLING_EDGE, RISING_EDGE };
+	public static int rotateSpeed = 50;
+	private Odometer odom;
+	private SampleProvider usValue;
+	private Navigation navigation;
+	private float[] usData;
+	private LocalizationType locType;
+	private EV3LargeRegulatedMotor leftMotor, rightMotor;
+	private int wallDis = 30;
+
+	
+	public USLocalizer(SampleProvider usValue, float[] usData, EV3LargeRegulatedMotor leftMotor, 
+			EV3LargeRegulatedMotor rightMotor, Navigation navigation, Odometer odom, LocalizationType locType) {
+		this.odom = odom;
+		this.usValue = usValue;
+		this.usData = usData;
+		this.locType = locType;
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;
+		this.navigation = navigation;
+	}
+	
+	
+	public void doLoc(){
+		double angleA, angleB, adjAngle;	
+		
+		//set the speed
+		leftMotor.setSpeed(rotateSpeed);
+		rightMotor.setSpeed(rotateSpeed);
+
+	
+		
+		
+	}
 }
--- Lab 4 - Localization/Lab 4 Localization/src/localization/lab4.java
+++ Lab 4 - Localization/Lab 4 Localization/src/localization/lab4.java
@@ -12,7 +12,8 @@ public class lab4 {
 	private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
 	private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
 	private static final Port usPort = LocalEV3.get().getPort("S2");		
-	private static final Port lightPort = LocalEV3.get().getPort("S1");		
+	private static final Port lightPort = LocalEV3.get().getPort("S1");	
+	public static final double axleWidth = 12.7, wheelRadius = 2.125;
 	
 	public static void main(String[] args) {
 		// US sensor setting up
@@ -26,12 +27,31 @@ public class lab4 {
 		SensorModes lightSensor = new EV3ColorSensor(lightPort);
 		SampleProvider lightValue = lightSensor.getMode("Red");			// provides samples from this instance
 		float[] lightData = new float[lightValue.sampleSize()];			// buffer in which data are returned
+		
+		
+		//set up odometer, navigation, usLoc and lsLoc, LcdInfo
+		
+		
+	
 				
+		USLocalizer usLoc = new USLocalizer(usValue, usData, leftMotor, rightMotor, navigation, 
+				odom, USLocalizer.LocalizationType.RISING_EDGE);
+		
+		LSLocalizer lsLoc = new LSLcalizer();
+		
+		
+		
+		usLoc.doLoc();
+		while(Button.waitForAnyPress() != 1){
+			lsLoc.doLoc();
+		}
+		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+		System.exit(0);	
+		
+	
+
 		
 		
 	}
-	
-	
-	
 
 }
